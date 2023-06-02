daf24f3d131635960f381c533ba2934f5c758dbc
Pedro <pedrosprandel27@gmail.com>
2023-06-01 21:58:42 -0300

    listing-test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Todo list',
-}
+  title: "Todo list",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-zinc-800`}>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Home() {
-  return <></>
+import { prisma } from "@/db";
+
+export default async function Home() {
+  const todos = await prisma.todo.findMany();
+
+  return (
+    <body>
+      <header className="flex items-center justify-center p-16 mb-8">
+        <h1 className="text-6xl">Tasks</h1>
+      </header>
+      <main className="flex items-center justify-center">
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      </main>
+    </body>
+  );
 }
